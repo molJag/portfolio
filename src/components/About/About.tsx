dd1398112f1efc23677035f4dbc4f9ba05609bee
Johnny Aguilera <john5770@hotmail.com>
2025-01-24 15:33:53 +0100

    feat(portfolio): cosmetic changes

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -5,14 +5,16 @@ function About() {
   return (
     <section id="about" className="about">
       <div className="separator"></div>
-      <h2 className="about__title">
-        <span className="highlight">{mockData.about} </span>
-        {mockData.title}
-      </h2>
-
       <div className="about__content">
-        <p>{mockData.description}</p>
-        <p>{mockData.description2}</p>
+        <h2 className="about__title">
+          <span className="highlight">{mockData.about} </span>
+          {mockData.title}
+        </h2>
+
+        <div className="about__content-text">
+          <p>{mockData.description}</p>
+          <p>{mockData.description2}</p>
+        </div>
       </div>
     </section>
   );
